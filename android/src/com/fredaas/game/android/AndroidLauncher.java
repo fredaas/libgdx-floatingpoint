99c76d97068dfbf1dfb41f13ba18e63bc20a2f90
fredaas <fredaas86@gmail.com>
2016-01-01 16:54:56 +0100

    Add Android version.

--- android/src/com/fredaas/game/android/AndroidLauncher.java
+++ android/src/com/fredaas/game/android/AndroidLauncher.java
@@ -1,10 +1,9 @@
 package com.fredaas.game.android;
 
-import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.fredaas.game.Game;
+import android.os.Bundle;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
